ec1594ac39e5696482eb6ac7e4b8ae1f2fb7d8cd
Rob Flynn <rob@thingerly.com>
2019-06-09 22:56:47 -0400

    Add some notice view markup

--- app/javascript/packs/components/notice_view.vue
+++ app/javascript/packs/components/notice_view.vue
@@ -1,6 +1,83 @@
 <template>
   <div>
-    I am the notice view
+    <section class="box">
+      <h1>$$Notice Title$$ | Notification</h1>
+
+      <div>
+
+        <!-- metadata table -->
+        <!-- TODO: Make me a component -->
+        <table class="metadata">
+          <tbody>
+            <tr>
+              <th scope="col">
+                Case Name
+              </th>
+              <td>$$case_name$$</td>
+            </tr>
+            <tr>
+              <th scope="col">
+                Case Number
+              </th>
+              <td>$$case_number$$</td>
+            </tr>
+            <tr>
+              <th scope="col">
+                State
+              </th>
+              <td>$$state$$</td>
+            </tr>
+            <tr>
+              <th scope="col">
+                Document Number
+              </th>
+              <td>$$document_number$$</td>
+            </tr>
+            <tr>
+              <th scope="col">
+                Attorney
+              </th>
+              <td>$$attorney$$</td>
+            </tr>
+            <tr class="full">
+              <td colspan="2">
+                $$body$$
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+
+    <section class="box">
+      <h1>Documents | Upload</h1>
+
+      <div>
+        <DocumentList>
+          <div class="col document">
+            <img style="width: 140px; height: 175px;"/>
+
+            <button>Download</button>
+          </div>
+          <div class="col document">
+            <img style="width: 140px; height: 175px;"/>
+
+            <button>Download</button>
+          </div>
+          <div class="col document">
+            <img style="width: 140px; height: 175px;"/>
+
+            <button>Download</button>
+          </div>
+          <div class="col document">
+            <img style="width: 140px; height: 175px;"/>
+
+            <button>Download</button>
+          </div>
+        </DocumentList>
+      </div>
+    </section>
+
   </div>
 </template>
 
