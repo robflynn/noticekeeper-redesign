d5231766867e40cc653e404e6bbfa717c878edfa
Rob Flynn <rob@thingerly.com>
2019-09-01 04:18:30 -0400

    remove old placeholder property

--- app/serializers/court_case_serializer.rb
+++ app/serializers/court_case_serializer.rb
@@ -3,9 +3,6 @@ class CourtCaseSerializer < ActiveModel::Serializer
 
   attributes :id, :status, :case_name, :case_number,
              :case_state, :case_type, :created_at,
-             :updated_at, :notices
+             :updated_at
 
-  def notices
-    []
-  end
 end
